f74d1e753088436e1f1067ff0de9c9315f20bea7
Julio Moro Sama <49278008+xSamaa@users.noreply.github.com>
2020-10-15 00:24:42 +0200

    LoginActivity

---
+++ app/src/main/java/com/foc/firstgame/Login.java
@@ -0,0 +1,25 @@
+package com.foc.firstgame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+
+    }
+
+    public void login(View view) {
+        Intent i = new Intent(this, Menu.class);
+        startActivity(i);
+    }
+}
